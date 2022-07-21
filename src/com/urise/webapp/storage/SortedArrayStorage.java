6cc0d5c96932939f7ac08c9a2d0846b0522c1fac
Dez-Gard <dezgard.post@gmail.com>
2022-07-21 14:20:05 +0300

    HW3

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -9,14 +9,14 @@ public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
     protected void insertElement(Resume r, int index) {
-        int insertIdx = -index - 1;
+        int insertIdx = - (index + 1);
         System.arraycopy(storage, insertIdx, storage, insertIdx + 1, size - insertIdx);
         storage[insertIdx] = r;
     }
 
     @Override
     protected void fillDeletedElement(int index) {
-        int numMoved = size - index - 1;
+        int numMoved = size - (index + 1);
         if (numMoved > 0) {
             System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
